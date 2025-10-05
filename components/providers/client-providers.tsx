b18e537dc57c46fcb8428c606d374be1a02f6791
User <user@example.com>
2025-10-06 01:40:57 +0300

    Simplify client components to basic versions

--- components/providers/client-providers.tsx
+++ components/providers/client-providers.tsx
@@ -1,16 +1,14 @@
 'use client'
 
 import { AccessibilityButton } from '@/components/accessibility/accessibility-button'
-import { AdvancedPerformanceMonitor } from '@/components/performance/advanced-performance-monitor'
-import { ErrorMonitor, ErrorBoundary } from '@/components/error/error-monitor'
-import { QuickPerformance } from '@/components/performance/quick-performance'
+import AdvancedPerformanceMonitor from '@/components/performance/advanced-performance-monitor'
+import ErrorMonitor from '@/components/error/error-monitor'
+import QuickPerformance from '@/components/performance/quick-performance'
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <ErrorBoundary>
-        {children}
-      </ErrorBoundary>
+      {children}
       <AccessibilityButton />
       <AdvancedPerformanceMonitor />
       <ErrorMonitor />
